6f48e6c9435bdc75b4a3a30e7024cfe15a3d21da
Honlan <honlan@HonlandeMacBook-Air.local>
2016-08-23 17:07:18 +0800

    add codes for autoencoder

---
+++ auto-encoder/4_denoising_autoencoder.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding:utf8
+
+import sys 
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+from keras.datasets import mnist
+import numpy as np
+
+(x_train, _), (x_test, _) = mnist.load_data()
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = np.reshape(x_train, (len(x_train), 1, 28, 28))
+x_test = np.reshape(x_test, (len(x_test), 1, 28, 28))
+
+noise_factor = 0.5
+x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape) 
+x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape) 
+
+x_train_noisy = np.clip(x_train_noisy, 0., 1.)
+x_test_noisy = np.clip(x_test_noisy, 0., 1.)
+
+import matplotlib.pyplot as plt
+
+n = 10
+plt.figure(figsize=(20, 2))
+for i in range(n):
+    ax = plt.subplot(1, n, i + 1)
+    plt.imshow(x_test_noisy[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
+
+input_img = Input(shape=(1, 28, 28))
+
+x = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(input_img)
+x = MaxPooling2D((2, 2), border_mode='same')(x)
+x = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(x)
+encoded = MaxPooling2D((2, 2), border_mode='same')(x)
+
+# at this point the representation is (32, 7, 7)
+
+x = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(encoded)
+x = UpSampling2D((2, 2))(x)
+x = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(x)
+x = UpSampling2D((2, 2))(x)
+decoded = Convolution2D(1, 3, 3, activation='sigmoid', border_mode='same')(x)
+
+autoencoder = Model(input_img, decoded)
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+autoencoder.fit(x_train_noisy, x_train,
+                nb_epoch=100,
+                batch_size=128,
+                shuffle=True,
+                validation_data=(x_test_noisy, x_test))
+
+decoded_imgs = autoencoder.predict(x_test_noisy)
+
+import matplotlib.pyplot as plt
+
+n = 10
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # display original
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(x_test_noisy[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
